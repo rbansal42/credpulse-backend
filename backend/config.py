39eb7493c133bdeb47f3e4e9123a09f829647e69
Rahul Bansal <rahul.b@siyaconsultinggroup.com>
2024-11-12 18:32:38 +0530

    Refactoring

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 def config(filename='./database.ini', section='postgresql'):
-    db = {}
+    db_config = {}
     try:
         # create a parser
         parser = ConfigParser()
@@ -13,7 +13,7 @@ def config(filename='./database.ini', section='postgresql'):
         if parser.has_section(section):
             params = parser.items(section)
             for param in params:
-                db[param[0]] = param[1]
+                db_config[param[0]] = param[1]
         else:
             raise Exception(f'Section {section} not found in the {filename} file')
 
@@ -22,10 +22,10 @@ def config(filename='./database.ini', section='postgresql'):
     except Exception as e:
         print(f"An error occurred: {e}")
     finally:
-        if not db:
+        if not db_config:
             print(f"No configuration found for section '{section}' in {filename}.")
         else:
             print(f"Configuration loaded successfully from section '{section}' in {filename}.")
         print("Finished reading configuration.")
 
-    return db
+    return db_config
