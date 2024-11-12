6c4e5a1d7dfda2567058d39aa4392097a243cb03
Rahul Bansal <rahul.b@siyaconsultinggroup.com>
2024-11-12 19:23:19 +0530

    Handling DB as a source

--- backend/utils.py
+++ backend/utils.py
@@ -1,20 +1,22 @@
 import os
-from ingestion import csv_source_handler
-# from ingestion.db_source_handler
-# from ingestion.df_to_db import df_to_db
+import json
+import pandas as pd
+from datetime import datetime
+import ingestion, config, connect
 
 
 # Function to process the file based on its extension
 def file_type_handler(file_path):
     # Get the file extension (lowercased for consistency)
     file_extension = os.path.splitext(file_path)[1].lower()
-    
 
     if file_extension == '.json':
-        return csv_source_handler.csv_handler(file_path)
+        return ingestion.csv_source_handler.csv_handler(file_path)
     elif file_extension == '.ini':
-        return ".ini"
+        source_db_config = config(file_path)
+        return ingestion.db_source_handler.db_handler(source_db_config)
     elif file_extension == 3:
         return "Option 3 selected"
     else:
         return "Invalid option"
+
