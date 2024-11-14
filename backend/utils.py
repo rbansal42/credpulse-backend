9049eeeddf931c63e3ca0193d6e90c555541b499
Rahul Bansal <rahul.b@siyaconsultinggroup.com>
2024-11-14 20:06:58 +0530

    Refactoring and Logging

--- backend/utils.py
+++ backend/utils.py
@@ -2,19 +2,23 @@ import os
 import json
 import pandas as pd
 from datetime import datetime
-import ingestion, config, connect
+import config
+import ingestion.csv_source_handler
+import ingestion.db_source_handler
 
 
 # Function to process the file based on its extension
 def file_type_handler(file_path):
     # Get the file extension (lowercased for consistency)
     file_extension = os.path.splitext(file_path)[1].lower()
+    print('File Extension is:', file_extension)
 
     if file_extension == '.json':
         return ingestion.csv_source_handler.csv_handler(file_path)
     elif file_extension == '.ini':
-        source_db_config = config(file_path)
-        return ingestion.db_source_handler.db_handler(source_db_config)
+        print('Parsing Database Configuration file..')
+        source_db_config = config.parser(file_path)
+        return ingestion.db_source_handler.db_handler(connection_params=source_db_config)
     elif file_extension == 3:
         return "Option 3 selected"
     else:
